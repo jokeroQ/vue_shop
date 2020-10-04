5c7c408b2ebc12487c86465bd28eb63d44001d23
jokeroQ <8095785+jokeroQ@user.noreply.gitee.com>
2020-10-04 11:40:03 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Form, FormItem, Input, Button, Message } from 'element-ui'
+// 导入弹框提示组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
